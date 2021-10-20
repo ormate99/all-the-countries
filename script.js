2542aec92fe74e7ee7ac6b8d07e5794bcc43d9d8
ormate99 <ormate99@gmail.com>
2021-10-20 14:29:57 +0200

    all countries listed

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 async function loadEvent() {
     const rootElement = document.getElementById('root');
 
-    const res = await fetch("https://restcountries.com/v3.1/name/peru");
-    const peru = await res.json();
+    const res = await fetch("https://restcountries.com/v3.1/all");
+    const countries = await res.json();
 
-    console.log(peru);
+    console.log(countries);
+
+    for (const country of countries) {
+        const countryHTML = `
+        <section class="independent">
+            <img src="${country.flags.png}" alt="flag">
+            <h1>${country.name.official}</h1>
+            <h2>${country.capital}</h2>
+        </section>
+        `;
+
+        rootElement.insertAdjacentHTML('beforeend', countryHTML);
+    }
+
+  /*  console.log(peru);
     
-    const peruO = peru[0];
-    console.log(peruO);
+    const country = peru[0];
+    console.log(country);
 
     const peruHTML = `
         <section class="independent">
-            <img src="${peruO.flags.png}" alt="flag">
-            <h1>${peruO.name.official}</h1>
-            <h2>${peruO.capital}</h2>
+            <img src="${country.flags.png}" alt="flag">
+            <h1>${country.name.official}</h1>
+            <h2>${country.capital}</h2>
             <ul>
                 <li>First language</li>
             </ul>
@@ -23,7 +37,7 @@ async function loadEvent() {
 
     console.log(rootElement);
     
-    rootElement.insertAdjacentHTML('beforeend', peruHTML); 
+rootElement.insertAdjacentHTML('beforeend', peruHTML);*/
 }
 
 window.addEventListener("load", loadEvent);
